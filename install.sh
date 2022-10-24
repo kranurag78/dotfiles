f325bd92dcb58a2575ee774feeaa442064e0d5ac
kranurag78 <84301342+kranurag78@users.noreply.github.com>
2022-10-24 16:37:54 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 curl -sLS https://get.arkade.dev | sudo sh
+echo "export PATH=$PATH:$HOME/.arkade/bin" >> ~/.bashrc
+source ~/.bashrc
+export PATH=$PATH:$HOME/.arkade/bin
+arkade get kind 
+arkade get kubectl 
+kind create cluster 
 alias k=kubectl
-alias g=git 
-alias gst=git status
